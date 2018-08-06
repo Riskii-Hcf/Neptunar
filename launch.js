dbb2c916b294a766ed26430f3c739d479aa1be0f
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-08-07 06:07:48 +0800

    Create launch.js

---
+++ launch.js
@@ -0,0 +1,249 @@
+const Discord = require("discord.js");
+
+
+const TOKEN =  "Mzg5NDg5Mjc3MzU4MDQ3MjY1.DRSZ7Q.EOCHuFln8hB4yj2PxgW5bDCJW3o"
+const PREFIX = "-"
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+  bot.user.setGame(`-help | Neptunar | ${bot.guilds.size} servers!`,'https://twitch.tv/123silly');
+    console.log(`Started bot as: ${bot.user.tag}!`);
+});
+
+bot.on("guildMemberAdd", function(member) {
+member.guild.channels.find("name","main-chat").sendMessage(member.toString() + " Welcome to the server! Make sure to read rules so you dont get punished!");
+console.log(`[SPY] A member joined a server!`)
+member.addRole(member.guild.roles.find("name", "[Private]"));
+});
+
+bot.on("message", function(message) {
+if (message.author.equals(bot.user)) return;
+ 
+if (!message.content.startsWith(prefix)) return;
+ 
+var args = message.content.substring(prefix.length).split(" ");
+ 
+switch (args[0].toLowerCase()) {
+ 
+case "emojis":
+const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
+message.channel.send(emojiList);
+break;
+case "8ball":
+if (args[1]) {
+  message.channel.sendMessage(':8ball: ' + message + '? - `' + eb[Math.floor(Math.random() * eb.length)] + '`');
+} else {
+  message.channel.sendMessage("Please provide a question");
+}
+break;	
+case "roast":
+if (args[1]) {
+  message.channel.sendMessage(':fire: ' + ' - ' + r[Math.floor(Math.random() * r.length)] + '');
+} else {
+  message.channel.sendMessage("Please provide a UserName Please");
+}
+break;
+
+case "coinflip":
+var result = Math.floor((Math.random() * 2) + 1);
+    if (result === 1) {
+    message.reply('The coin landed on heads');
+} else if (result === 2) {
+message.reply('The coin landed on tails');
+}
+break;
+		
+case "help":
+message.delete("help")
+console.log(`Help message has been sent to <@${message.author.id}> !`)
+message.channel.sendMessage(`<@${message.author.id}> -> **Commands List** has been sent to your pm/dm!`)
+var embedt = new Discord.RichEmbed()
+.setTitle("All Commands")
+.setDescription('These Commands Will Help You!')
+.setThumbnail ('https://ak4.picdn.net/shutterstock/videos/10035044/thumb/1.jpg')
+.addField(`!8ball`, `:8ball: Gives A Answer!`)
+.addField(`!ping`, `Shows A 99% Correct Ping.`)
+.addField(`!emojis`, `Shows A L Emoji / Image.`)
+.addField(`!say`, `Say Whatver You Said.`)
+.addField(`!animesearch`, `Search The Anime You Like.`)
+.addField(`!mc [player]`, `Shows Minecraft Info.`)
+.addField(`!ban [user]`, `Bans A User.`)
+.addField(`!kick [user]`, `Kicks A User.`)
+.addField(`!mute [user]`, `Mutes A User.`)
+.addField(`!umute [user]`, `Unmutes A User.`)
+.setFooter("PayBotV2 [Help]")
+.setColor(0x1ab517)
+message.author.sendEmbed(embedt)
+break;
+
+case "ping":
+message.reply(`Pong! ${Math.round(bot.ping)}ms`);
+break;
+
+case "invite":
+message.reply(`Please Invite Me Here! \https://discordapp.com/api/oauth2/authorize?client_id=420323901034201089&permissions=2146958583&scope=bot`);
+break;
+		
+case "say":
+    var sayargs = message.content.substring(5).split(" ");
+                    const saymsg = sayargs.join(" ");
+                    message.delete().catch(O_o=>{});
+		    message.channel.sendMessage(saymsg);
+  break;
+
+case "animesearch":
+if (args[1]) {
+    message.reply(`https://www.animepower.net/` + args[1]);
+} else {
+    message.reply(`How to use: \n${prefix}searchanime [anime] **NOTICE: DONT USE SPACES, USE - =**`);
+}
+break;
+
+        case "kick":
+    let modRoles = message.guild.roles.find("name", "Moderator");
+        if (args[1]) {
+    if(message.member.roles.has(modRoles.id)) {
+      let kickMember = message.guild.member(message.mentions.users.first());
+      message.guild.member(kickMember).kick();
+      message.channel.sendMessage(":fire: You Have been Kicked on Riskii's Discord :fire:");
+      message.mentions.users.first().send(`:x: **You have been KICKED on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+    } else {
+      return message.reply(":x: **You dont have permissions to do that!** :x:");
+    }
+    } else {
+    return message.reply(`:x: **Please mention a user** :x:`);
+    }
+break;
+case "ban":
+    let modRolesss = message.guild.roles.find("name", "Moderator");
+        if (args[1]) {
+    if(message.member.roles.has(modRolesss.id)) {
+      let banMember = message.guild.member(message.mentions.users.first());
+      message.guild.member(banMember).ban();
+      message.channel.sendMessage(":fire: You Have been banned on Riskii's Discord :fire:");
+      message.mentions.users.first().send(`:x: **You have been PERM BANNED! on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+    } else {
+      return message.reply(":x: **You dont have permissions to do that!** :x:");
+    }
+    } else {
+    return message.reply(`:x: **Please mention a user** :x:`);
+    }
+break;
+case "mute":
+let modRolez = message.guild.roles.find("name", "Moderator");
+if(message.member.roles.has(modRolez.id)) {
+ let muteMember = message.guild.member(message.mentions.users.first());
+ muteMember.addRole(muteMember.guild.roles.find("name", "Muted"));
+ message.channel.sendMessage(":fire: You Have been Muted on Riskii's Discord :fire:");
+ message.mentions.users.first().send(`:x: **You have been MUTED on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+}
+else {
+ return message.reply("=mute [player]");
+}
+break;
+case "unmute":
+let modRolezz = message.guild.roles.find("name", "Moderator");
+if (args[1]) {
+if(message.member.roles.has(modRolezz.id)) {
+let unMember = message.guild.member(message.mentions.users.first());
+unMember.removeRole(unMember.guild.roles.find("name", "Muted"));
+message.channel.sendMessage(":fire: You Have been Unmuted on Riskii's Discord :fire:");
+message.mentions.users.first().send(`:tada: **You have been unmuted on ${message.guild.name}! :wink: Please contact <@${message.author.id}> for more information!** :tada:`)
+} else {
+  return message.reply(":x: **You dont have permissions to do that!** :x:");
+}
+} else {
+return message.reply(`=unmute [player]`);
+}
+break;
+case "warn":
+let modRoless = message.guild.roles.find("name", "Moderator");
+if (args[1]) {
+if(message.member.roles.has(modRoless.id)) {
+let wMember = message.guild.member(message.mentions.users.first());
+message.channel.sendMessage(":fire: You Have been Warned on Riskii's Discord :fire:");
+message.mentions.users.first().send(`:x: **You have been warned on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+}
+else
+{
+  return message.reply(":x: **You dont have permissions to do that!** :x:");
+  } } else {
+      return message.reply(`=mute [player]`);
+  }
+break;
+case "unwarn":
+let modRolex = message.guild.roles.find("name", "Moderator");
+if (args[1]) {
+if(message.member.roles.has(modRolex.id)) {
+let uMember = message.guild.member(message.mentions.users.first());
+message.channel.sendMessage(":arrow_right: The Member Has Been Unwarned! :point_left:");
+ message.mentions.users.first().send(`:tada: **You have been unwarned on ${message.guild.name}! :wink: Please contact <@${message.author.id}> for more information!** :tada:`)
+}
+else
+{
+  return message.reply(":x: **You dont have permissions to do that!** :x:");
+}
+} else {
+return message.reply(`:x: **Please mention a user** :x:`);
+}
+break;
+case "mc":
+var mc = new Discord.RichEmbed()
+.setTitle("Minecraft information")
+.setThumbnail('https://crafatar.com/renders/head/' + args[1] + '?helm&scale=10')
+.setDescription('information about ' + args[1])
+.addField('Username:', '' + args[1])
+.addField(`Skin:`, `https://namemc.com/` + args[1] + '&s=700')
+.addField('NameMc:', 'https://namemc.com/name/' + args[1])
+.setFooter('Latenci')
+message.channel.sendEmbed(mc)
+break;
+		
+  case "marry":
+    let mar = message.mentions.users.first();
+    if (!mar) {
+      message.reply("Please mention a user to marry.");
+    } else {
+    var mare = new Discord.RichEmbed()
+    .setTitle(`New people has been married! :heart_eyes: :heart_eyes: `)
+    .setDescription(`:heart: <@${message.author.id}> is now married with <@${mar.id}>! :wink:`)
+    .setImage(`https://i.ytimg.com/vi/6ytPCSS8_7s/maxresdefault.jpg`)
+    .setColor(0x721487)
+    message.channel.sendEmbed(mare);
+    }
+    break;
+		
+case "clear":
+if (args[1]) {
+    if (args[1] > 2) {
+        if (args[1] > 50) {
+            message.reply("You are clearing too much messages!");
+        }
+        else {
+let messagecount = parseInt(args[1]);
+  message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+        }
+    } else {
+        message.reply("You can only delete 3 messages or more.");
+    }
+} else {
+    message.reply("Please type number of messages to delete.");
+}
+break;
+		
+	
+ 
+ 
+ 
+ 
+ 
+}
+});
+ 
+ 
+ 
+ 
+ 
+ 
+bot.login(process.env.BOT_TOKEN);
